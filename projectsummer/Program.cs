d06d49b9ca872ff6745ff2ef848a0ae8ff806f64
maxmanusv1 <60714489+maxmanusv1@users.noreply.github.com>
2020-07-03 20:03:49 +0300

    Update Program.cs
    now works better, also decreased code

--- projectsummer/Program.cs
+++ projectsummer/Program.cs
@@ -16,75 +16,45 @@ namespace projectsummer
     
     class Program
     {
+        internal static void SaveLogs(ArrayList logs)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Would you like to save logs? Y/N");
+            string get = Console.ReadLine();
+            if (get.ToLower() == "y")
+            {
+                Console.WriteLine("Logs are starting to save!");
+                string[] myArray = (string[])logs.ToArray(typeof(string));
+                File.WriteAllLines("logs.txt", myArray);
+                Console.WriteLine("Logs are saved");
+            }
+            else
+                Console.WriteLine("Logs will not saved");
+        }
         class Search
         {
-            internal static void SaveLogs(ArrayList logs)
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Would you like to save logs? Y/N");
-                string get = Console.ReadLine();
-                if (get.ToLower() == "y")
-                {
-                    Console.WriteLine("Logs are starting to save!");
-                    string[] myArray = (string[])logs.ToArray(typeof(string));
-                    File.WriteAllLines("logs.txt", myArray);
-                    Console.WriteLine("Logs are saved");
-                }
-                else
-                    Console.WriteLine("Logs will not saved");
-            }
-            internal static void SimpleSearch(string[] link,string[] extensions)
-            {
-                ArrayList list = new ArrayList();
-                Console.WriteLine("\n Starting...");
-              
-                for (int i = 0; i < link.Length; i++)
-                {
-                    for (int z = 0; z <= 30; z++)
-                    {
-                        try
-                        {
-                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link[i] + extensions[z]);
-                            var response = request.GetResponse();
-                            request.Abort();
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(" Found! " + link[i] + extensions[z]);
-                            list.Add(link[i] + extensions[z]);
-                        }
-                        catch
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(" Not found! " + link[i] + extensions[z]);
-                        }
-                    }
-                    Console.WriteLine("\n");
-                }
-                Console.WriteLine("Searching succesfully done");
-                SaveLogs(list);
-             
-            }
-            internal static void AdvancedSearchOption1(string[] link, string[] extensions)
+            internal static void Search_(string[] link2, string[] extensions2,int length)
             {
                 ArrayList list = new ArrayList();
                 Console.WriteLine("\n Starting...");
 
-                for (int i = 0; i < link.Length; i++)
+                for (int i = 0; i < link2.Length; i++)
                 {
-                    for (int z = 0; z <= extensions.Length; z++)
+                    for (int z = 0; z <= length; z++)
                     {
                         try
                         {
-                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link[i] + extensions[z]);
+                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link2[i] + extensions2[z]);
                             var response = request.GetResponse();
                             request.Abort();
                             Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(" Found! " + link[i] + extensions[z]);
-                            list.Add(link[i] + extensions[z]);
+                            Console.WriteLine(" Found! " + link2[i] + extensions2[z]);
+                            list.Add(link2[i] + extensions2[z]);
                         }
                         catch
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(" Not found! " + link[i] + extensions[z]);
+                            Console.WriteLine(" Not found! " + link2[i] + extensions2[z]);
                         }
                     }
                     Console.WriteLine("\n");
@@ -92,52 +62,43 @@ namespace projectsummer
                 Console.WriteLine("Searching succesfully done");
                 SaveLogs(list);
             }
-            internal static void AdvancedSearchOption2(string[] link, string[] extensions)
+            internal static void SimpleSearch(string[] link,string[] extensions,int length)
             {
-                ArrayList list = new ArrayList();
-                Console.WriteLine("\n Starting...");
-                for (int i = 0; i < link.Length; i++)
-                {
-                    for (int z = 0; z <= extensions.Length; z++)
-                    {
-                        try
-                        {
-                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link[i] + extensions[z]);
-                            var response = request.GetResponse();
-                            request.Abort();
-                            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(" Found! " + link[i] + extensions[z]);
-                            list.Add(link[i] + extensions[z]);
-                        }
-                        catch
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(" Not found! " + link[i] + extensions[z]);
-                        }
-                    }
-                    Console.WriteLine("\n");
-                }
-                Console.WriteLine("Searching succesfully done");
-                SaveLogs(list);
+                Search_(link,extensions,length);
+            }
+            internal static void AdvancedSearchOption1(string[] link, string[] extensions,int length)
+            {
+                Search_(link, extensions,extensions.Length);
+            }
+            internal static void AdvancedSearchOption2(string[] link, string[] extensions,int length)
+            {
+                Search_(link, extensions,extensions.Length);
             }
         }
         class GetWebsite
         {
-            internal static string[] Sorgula()
+           internal static string[] Sorgula()
             {
                 Sorgu:
-                Console.WriteLine(" If you want to use for just one site press 1, \n If you will use multiple sites from text file press 2");
-                int a = int.Parse(Console.ReadLine());
-                switch (a)
+                try
                 {
-                    case 1:
-                        return GetOne();
-                    case 2:
-                       return GetFromFile();
-                    default:
-                        goto Sorgu;
+                    Console.WriteLine(" If you want to use for just one site press 1, \n If you will use multiple sites from text file press 2");
+                    int a = int.Parse(Console.ReadLine());
+                    switch (a)
+                    {
+                        case 1:
+                            return GetOne();
+                        case 2:
+                            return GetFromFile();
+                        default:
+                            goto Sorgu;
+                    }
                 }
+                catch 
+                {
+                    goto Sorgu;
+                }
+               
             }
             internal static string[] GetOne()
             {
@@ -177,19 +138,19 @@ namespace projectsummer
             Console.WriteLine("Which search type do you want to use? \n 1- Simple Search Total 30 Keywords  \n 2- Advanced Search Total "+kes.Length +" Keywords \n 3- Advanced Search-2 Total "+kes2.Length +" Keywords ");
             string al = Console.ReadLine();
             string[] siteler;
-            switch (al)
+           switch (al)
             {
                 case "1":
                     siteler = GetWebsite.Sorgula();
-                    Search.SimpleSearch(siteler,kes);
+                    Search.SimpleSearch(siteler,kes,30);
                     break;
                 case "2":
                     siteler = GetWebsite.Sorgula();
-                    Search.AdvancedSearchOption1(siteler,kes);
+                    Search.AdvancedSearchOption1(siteler,kes,kes.Length);
                     break;
                 case "3":
                     siteler = GetWebsite.Sorgula();
-                    Search.AdvancedSearchOption2(siteler,kes2);
+                    Search.AdvancedSearchOption2(siteler,kes2,kes2.Length);
                     break;
                 default:
                     Console.WriteLine("You wrote wrong number!");
